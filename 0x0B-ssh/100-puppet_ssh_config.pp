674f06ce567f92fce236d7bb73678e35c5e183a3
HabtB <email.Habtamu@gmail.com>
2023-03-26 22:16:47 +0000

    100 puppet updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,34 +1,13 @@
-
 # Define the resource type for the SSH client configuration file
-  file { '/home/vagrant/.ssh/config':
-    ensure => 'file',
-    owner => 'vagrant',
-    group => 'vagrant',
-    mode => '0600',
-    content => "
-	Host server1 
-    	    HostName 174.129.54.166
-            Port 22
-            IdentityFile  ~/.ssh/school
-            PasswordAuthentication no
-     ",
-}
-
-# Define the resource type for the private key file
-  file { '/home/vagrant/.ssh/school':
-    ensure => 'present',
-    owner => 'vagrant',
-    group => 'vagrant',
-    mode => '0600',
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
 }
 
 # Declare the identity file in global ssh configuration
-file { '/etc/ssh/ssh_config':
-  ensure => 'file',
-  owner => 'root',
-  group => 'root',
-  mode => '0644',
-  content => "
-    IdentityFile /home/vagrant/.ssh/school
-  ",
+file_line { 'Declare identity file':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile /home/vagrant/.ssh/school',
 }
