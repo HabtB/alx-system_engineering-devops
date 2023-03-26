b6ff8735f9ebde22765cdb23323a14bc472ea323
HabtB <email.Habtamu@gmail.com>
2023-03-26 22:28:39 +0000

    100 puppet updated

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,12 +2,11 @@
 file_line { 'Turn off passwd auth':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+  line   => '    PasswordAuthentication no',
 }
 
-# Declare the identity file in global ssh configuration
 file_line { 'Declare identity file':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile /home/vagrant/.ssh/school',
+  line   => '    IdentityFile ~/.ssh/school',
 }
