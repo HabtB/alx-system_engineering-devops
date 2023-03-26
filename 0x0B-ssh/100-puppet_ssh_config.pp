91bfcae8db24a3bb1bca3f7920ed162c72c0112d
HabtB <email.Habtamu@gmail.com>
2023-03-26 20:50:54 +0000

    100-puppet added

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,26 @@
+# Define the resource type for the SSH client configuration file
+  file { '/home/vagrant/.ssh/config':
+    ensure => 'file',
+    owner => 'vagrant',
+    group => 'vagrant',
+    mode => '0600',
+    content => "
+	Host server1 
+    	    HostName 174.129.54.166
+            Port 22
+            IdentityFile  ~/.ssh/school
+            PasswordAuthentication no
+     ",
+}
+
+# Define the resource type for the private key file
+  file { '/home/vagrant/.ssh/school':
+    ensure => 'file',
+    owner => 'vagrant',
+    group => 'vagrant',
+    mode => '0600',
+    content   => "
+	Host 
+	 IdentityFile ~/.ssh/school
+	",
+}
