aed73aefea55c58d4d2496e1f37e1c86b6d611b0
HabtB <email.Habtamu@gmail.com>
2023-04-25 02:31:09 +0000

    1 updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,49 +1,27 @@
 #!/usr/bin/python3
-import requests
-import sys
+'''
+Returns TODO list progress of an employee.
+'''
 
-users_url = "https://jsonplaceholder.typicode.com/users"
-todos_url = "https://jsonplaceholder.typicode.com/todos"
+if __name__ == '__main__':
+    import requests
+    import sys
 
+    NUMBER_OF_DONE_TASKS = 0
+    TASK_TITLE = []
+    USER_ID = sys.argv[1]
+    req = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                       format(USER_ID)).json()
+    EMPLOYEE_NAME = req.get("name")
+    req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                       format(USER_ID)).json()
+    for item in req:
+        if item.get('completed') is True:
+            TASK_TITLE.append(item.get('title'))
+            NUMBER_OF_DONE_TASKS += 1
+    TOTAL_NUMBER_OF_TASKS = len(req)
 
-def first_line(id):
-    """ Fetch number of tasks """
-
-    todos_count = 0
-    todos_done = 0
-
-    resp = requests.get(todos_url).json()
-    for i in resp:
-        if i['userId'] == id:
-            todos_count += 1
-        if (i['completed'] and i['userId'] == id):
-            todos_done += 1
-
-    employee_name = None
-    completed_tasks = []
-    total_number_of_tasks = 0
-    number_of_completed_tasks = 0
-
-    url = f"{users_url}/{id}"
-    employee_data = requests.get(url).json()
-    employee_name = employee_data['name']
-
-    url2 = f"{users_url}/{id}/todos"
-    tasks = requests.get(url2).json()
-    for task in tasks:
-        if task['completed'] is True:
-            completed_tasks.append(task)
-            number_of_completed_tasks += 1
-        total_number_of_tasks += 1
-
-    """ Print the employee TODO list progress """
-    print('Employee {} is done with tasks({}/{}):'.format(employee_name,
-          number_of_completed_tasks, total_number_of_tasks))
-
-    """Print the title of completed tasks"""
-    for task in completed_tasks:
-        print("\t {}".format(task["title"]))
-
-
-if __name__ == "__main__":
-    first_line(int(sys.argv[1]))
+    print('Employee {} is done with tasks({}/{}):'.
+          format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+    for title in TASK_TITLE:
+        print('\t {}'.format(title))
