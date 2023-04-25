b0e48abe2c76f19de32115b47b28b2c0981f5f1c
HabtB <email.Habtamu@gmail.com>
2023-04-25 02:19:43 +0000

    1 updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,39 @@
-#!/usr/bin/python3
-"""Makes a request to the REST API to get TODO list progress of employees"""
-
 import requests
 import sys
 
+users_url = "https://jsonplaceholder.typicode.com/users"
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/" + str(employee_id)
-    response = requests.get(url)
-    employee_data = response.json()
 
-    """ Get the employee name and the number of completed tasks"""
-    employee_name = employee_data['name']
-    url2 = url + "/todos"
-    response = requests.get(url2)
-    tasks = response.json()
-    number_of_completed_tasks = 0
+def first_line(id):
+    """ Fetch number of tasks """
+
+    todos_count = 0
+    todos_done = 0
+
+    resp = requests.get(todos_url).json()
+    for i in resp:
+        if i['userId'] == id:
+            todos_count += 1
+        if (i['completed'] and i['userId'] == id):
+            todos_done += 1
+
+    employee_name = None
     completed_tasks = []
+    total_number_of_tasks = 0
+    number_of_completed_tasks = 0
 
+    url = f"{users_url}/{id}"
+    employee_data = requests.get(url).json()
+    employee_name = employee_data['name']
+
+    url2 = f"{users_url}/{id}/todos"
+    tasks = requests.get(url2).json()
     for task in tasks:
         if task['completed'] is True:
             completed_tasks.append(task)
             number_of_completed_tasks += 1
-
-    """ Total Number of tasks """
-    total_number_of_tasks = len(tasks)
+        total_number_of_tasks += 1
 
     """ Print the employee TODO list progress """
     print('Employee {} is done with tasks({}/{}):'.format(employee_name,
