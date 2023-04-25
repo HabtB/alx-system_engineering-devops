b294f19fb96fe5b32c34408a4fa7507e7166aa49
HabtB <email.Habtamu@gmail.com>
2023-04-25 17:15:53 +0000

    export file added

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Makes a request to the REST API to get TODO list progress of employees"""
-
+import csv
 import requests
 import sys
-import csv
+
 
 if __name__ == '__main__':
     employee_id = sys.argv[1]
