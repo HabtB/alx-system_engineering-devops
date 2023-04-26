a38e88e98e3a57f56481d44b17cff6c37304a723
HabtB <email.Habtamu@gmail.com>
2023-04-26 17:26:22 +0000

    0 updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,30 @@
 #!/usr/bin/python3
-"""Makes a request to the REST API to get TODO list progress of employees"""
-
 import requests
 import sys
 
+if len(sys.argv) != 2:
+    print("Usage: python3 gather_data_from_an_API.py employee_id")
+    sys.exit(1)
 
-if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/users/" + str(employee_id)
-    response = requests.get(url)
-    employee_data = response.json()
+employee_id = sys.argv[1]
+url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
 
-    """ Get the employee name and the number of completed tasks"""
-    employee_name = employee_data['name']
-    url2 = url + "/todos"
-    response = requests.get(url2)
-    tasks = response.json()
-    number_of_completed_tasks = 0
-    completed_tasks = []
+response = requests.get("https://jsonplaceholder.typicode.com/users/"
+                        + str(employee_id))
+employee_data = response.json()
+employee_name = employee_data['name']
 
-    for task in tasks:
-        if task['completed'] is True:
-            completed_tasks.append(task)
-            number_of_completed_tasks += 1
+response = requests.get(url)
+if response.status_code != 200:
+    print('Error: API request unsuccessful.')
+    sys.exit(1)
 
-    """ Total Number of tasks """
-    total_number_of_tasks = len(tasks)
+todos = response.json()
+total_tasks = len(todos)
+done_tasks = sum(todo.get('completed') for todo in todos)
 
-    """ Print the employee TODO list progress """
-    print('Employee {} is done with tasks({}/{}):'.format(employee_name,
-          number_of_completed_tasks, total_number_of_tasks))
-
-    """Print the title of completed tasks"""
-    for task in completed_tasks:
-        print("\t {}".format(task["title"]))
+print('Employee {} is done with tasks({}/{}):'.
+      format(employee_name, done_tasks, total_tasks))
+for todo in todos:
+    if todo.get('completed'):
+        print('\t {}'.format(todo.get('title')))
