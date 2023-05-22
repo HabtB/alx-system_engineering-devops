94e05905ac583bea4f3a744a14b1f91f3b342cf5
HabtB <email.Habtamu@gmail.com>
2023-05-22 15:21:28 +0000

    0-gather_data_from_an_API.py added

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
-
 import requests
-from sys import argv
+import sys
 
+if len(sys.argv) != 2:
+    print("Usage: python3 gather_data_from_an_API.py employee_id")
+    sys.exit(1)
 
-if __name__ == '__main__':
-    e_Id = argv[1]
-    url = "https://jsonplaceholder.typicode.com/" + "users/{}".format(e_Id)
-    response = requests.get(url)
-    employeeName = response.json().get('name')
-    payload = {"userId": e_Id}
-    many_tasks = requests.get(url + '/todos/', params=payload).json()
+employee_id = sys.argv[1]
+url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(employee_id)
 
-    done = 0
-    done_tasks = []
+response = requests.get("https://jsonplaceholder.typicode.com/users/"
+                        + str(employee_id))
+employee_data = response.json()
+employee_name = employee_data['name']
 
-    for task in many_tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
+response = requests.get(url)
+if response.status_code != 200:
+    print('Error: API request unsuccessful.')
+    sys.exit(1)
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(many_tasks)))
+todos = response.json()
+total_tasks = len(todos)
+done_tasks = sum(todo.get('completed') for todo in todos)
 
-    for task in done_tasks:
-        print("     {}".format(task.get('title')))
+print('Employee {} is done with tasks({}/{}):'.
+      format(employee_name, done_tasks, total_tasks))
+for todo in todos:
+    if todo.get('completed'):
+        print('\t {}'.format(todo.get('title')))
