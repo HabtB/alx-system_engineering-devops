33938f6ee71b631e28377e7f696ac1dbbb86ce90
HabtB <email.Habtamu@gmail.com>
2023-06-07 21:34:29 +0000

    added a comment to explain the puppet at the beginning of the code

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,4 @@
+# This puppet script substitutes the type 'phpp' with 'php'
 exec { 'fix_wordpress_config/typo error':
   command => '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
   path    => '/bin:/usr/bin',
